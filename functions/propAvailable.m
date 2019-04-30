2012d4ff5846b9f691bbfec2ef6aa067ada1b5e4
Tim Bernhard <tim@bernhard-webstudio.ch>
2019-04-30 20:24:12 +0200

    fix propAvailable detecting table as object

--- functions/propAvailable.m
+++ functions/propAvailable.m
@@ -3,10 +3,10 @@ function availability = propAvailable(object, property)
 %   Detailed explanation goes here
     if (isstruct(object))
         availability = isfield(object, property);
-    elseif (isobject(object))
-        availability = isprop(object, property);
     elseif (istable(object))
         availability = ismember(property, object.Properties.VariableNames);
+    elseif (isobject(object))
+        availability = isprop(object, property);
     else
         warning('Cannot determine availability of property "%s" on object of class "%s".', property, class(object));
         try 
